8bfc42e2cd0ebf0040064b8ba5e1fecec7d1db9b
김대엽 <kdaeyoub@gmail.com>
2023-01-06 01:38:22 +0900

    code 11

--- server.py
+++ server.py
@@ -1,12 +1,67 @@
-from flask import Flask, render_template, request
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
+from flask_login import (
+    UserMixin,
+    current_user,
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+)
 import sqlite3 as sql
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.id = username
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def get(cls, username):
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("select * from users where name = ?", [username])
+            result = cur.fetchall()
+            if result == []:
+                return Nnoe
+            else:
+                username, password = result[0]
+                return cls(username, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("home.html")
+
+
+@app.route("/only-login-user/")
+@login_required
+def only_login_user():
+    return f"this is login user : {current_user.username}"
+
+
+@app.route("/signout/")
+@login_required
+def signout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/signin/", methods=["GET", "POST"])
@@ -26,6 +81,8 @@ def signin():
                 username, password = result[0]
 
             if password == request.form["password"]:
+                user = User(username, password)
+                login_user(user)
                 return "Login success"
             else:
                 return "Wrong password"
